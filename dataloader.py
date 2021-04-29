442dd6ff0ea2eafc17a8557fc48a778f02d33cc5
Jay Dulera <53439144+jaydulera@users.noreply.github.com>
2021-04-29 14:57:15 +0530

    Added Files

---
+++ dataloader.py
@@ -0,0 +1,70 @@
+import os 
+import numpy as np
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import torch.utils.data
+from PIL import Image
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+import yaml
+import argparse
+
+# parser = argparse.ArgumentParser(description='train maskrcnn.')
+# parser.add_argument("--config", help="Config file for Mask Rcn", default='./config.yaml')
+# args = parser.parse_args()
+
+class Dataset(torch.utils.data.Dataset):
+    
+    def __init__(self, root, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "JPEGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "SegmentationClass"))))
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, "JPEGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "SegmentationClass", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path)
+        mask = np.array(mask)
+        obj_ids = np.unique(mask)
+        obj_ids = obj_ids[1:]
+        masks = mask == obj_ids[:, None, None]
+        num_objs = len(obj_ids)
+        boxes = []
+        
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
---
+++ train.py
@@ -0,0 +1,127 @@
+import os 
+import numpy as np
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import torch.utils.data
+from PIL import Image
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+import yaml
+import dataloader
+import argparse
+
+os.system('git clone https://github.com/pytorch/vision.git')
+os.system('cd vision')
+os.system('git checkout v0.3.0')    
+os.system('cp references/detection/utils.py ../')
+os.system('cp references/detection/transforms.py ../')
+os.system('cp references/detection/coco_eval.py ../')
+os.system('cp references/detection/engine.py ../')
+os.system('cp references/detection/coco_utils.py ../')
+
+
+from engine import train_one_epoch, evaluate
+import utils
+import transforms as T
+
+parser = argparse.ArgumentParser(description='train maskrcnn.')
+parser.add_argument("--config", help="Config file for Mask Rcn", default='./config.yaml')
+args = parser.parse_args()
+print(args.config)
+
+        
+def get_instance_segmentation_model(num_classes):
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+    
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+
+    
+def main():
+
+    with open(args.config, 'r') as f:
+        yam = yaml.load(f)
+    dataset_path = yam['dataset_path']
+    test_dataset_path = yam['test_dataset_path']
+    batch_size = yam['batch_size']
+    epochs = yam['epochs']
+    gamma = yam['gamma']
+    lr =yam['lr']
+    momentum = yam['momentum']
+    num_classes = yam['num_classes']
+    num_workers = yam['num_workers'] 
+    step_size = yam['step_size']
+    weight_decay= yam['weight_decay']
+
+    dataset = dataloader.Dataset(dataset_path)
+    
+   
+
+    # %git clone https://github.com/pytorch/vision.git
+    # cd vision
+    # !git checkout v0.3.0
+    # cp references/detection/transforms.py ../
+    # cp references/detection/coco_eval.py ../
+    # cp references/detection/engine.py ../
+    # cp references/detection/coco_utils.py ../
+    # cp references/detection/utils.py ../
+
+    from engine import train_one_epoch, evaluate
+    import utils
+    import transforms as T
+
+    
+    dataset = dataloader.Dataset(dataset_path, get_transform(train=True))
+    dataset_test = dataloader.Dataset(test_dataset_path, get_transform(train=False))
+    
+    
+    data_loader = torch.utils.data.DataLoader(
+    dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
+    collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=batch_size, shuffle=False, num_workers=num_workers,
+        collate_fn=utils.collate_fn)    
+
+    model = get_instance_segmentation_model(num_classes)
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=lr,
+                                momentum=momentum, weight_decay=weight_decay)
+    
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=step_size,
+                                                   gamma=gamma)
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    for epoch in range(epochs):
+        train_one_epoch(model, optimizer, data_loader,  device,epoch, print_freq=10)
+        lr_scheduler.step()
+        print(evaluate(model, data_loader_test, device=device))     
+    
+    if __name__ == "__main__":
+        main()
